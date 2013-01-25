e646fdfc27eaac162aa583dc8ed5081a96b90e05
Olivier Poupeney <olivier@dreamface.org>
2013-01-25 07:51:31 +0100

    1st loop implementation

--- cbs_widget_publisher.js
+++ cbs_widget_publisher.js
@@ -5,5 +5,49 @@
 
 function cbsWidgetPublisher( dataWidget ) {
 	dataWidget.clearContent();
-	dataWidget.addContent( "Widget Publisher" );
+	
+	var dq = new DataQuery( "TestPublisher" );
+	dq.execute( null, function(dataSet) {
+		var buffer = dataSet.getData();
+		if ( buffer !== null && buffer["coResultVal"] !== null ) {
+			var items = buffer.coResultVal;
+			var publisher = new CBSPublisher(items);
+			
+			var parseContinue = true;
+			var parseIndex = 0;
+			var loopIndex = 0;
+			var loopLimit = 2000;
+			
+			while (parseContinue) {
+				loopIndex++;
+				parseIndex = publisher.parseItem( items[parseIndex], parseIndex );
+				if ( parseIndex >= items.length || loopIndex > loopLimit ) {
+					parseContinue = false;
+				}
+			}
+			dataWidget.addContent( publisher.reportName );
+	});
+	
+}
+
+function CBSPublisher(items) {
+	this.items = items;
+	this.reportName = null;
+	return this;
+}
+
+CBSPublisher.prototype.type="CBSPublisher";
+
+CBSPublisher.prototype.parseItem=function( item, index ) {
+	var nextIndex = index+1;
+	if ( item.dimName == "CR" ) {
+		this.setReportName( item.c01 );
+	} else if ( item.dimName == "CT" ) {
+		// todo
+	}
+	return nextIndex;
+}
+
+CBSPublisher.prototype.setReportName=function( name ) {
+	this.reportName = name;
 }
